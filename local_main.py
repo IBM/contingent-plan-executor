28a021a7da8eddd95affab7cfbb8bcad32f85bd1
beckydvn <18rldv@queensu.ca>
2022-07-22 16:20:42 -0400

    cleanup by deleting old api calls

--- local_main.py
+++ local_main.py
@@ -12,34 +12,6 @@ import json
 
 import requests
 
-# city="Kingston"
-# url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily"
-
-# querystring = {"city":city, "country": "Canada"}
-
-# headers = {
-# 	"X-RapidAPI-Key": "b2c27ee596mshd8717ee1c0eba27p1c6ff4jsn1467f4601dfe",
-# 	"X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
-# }
-
-# response = requests.request("GET", url, headers=headers, params=querystring)
-
-# print(response.text)
-
-# url = "https://hotels-com-provider.p.rapidapi.com/v1/hotels/search"
-
-# # querystring = {"checkin_date":"2022-08-26","checkout_date":"2022-09-27","sort_order":"STAR_RATING_HIGHEST_FIRST","destination_id":"549499","adults_number":"1","locale":"en_US","currency":"USD","children_ages":"4,0,15","price_min":"10","star_rating_ids":"3,4,5","accommodation_ids":"20,8,15,5,1","price_max":"500","page_number":"1","theme_ids":"14,27,25","amenity_ids":"527,2063","guest_rating_min":"4"}
-# querystring = {"checkin_date":"2022-08-26","checkout_date":"2022-09-27","sort_order":"STAR_RATING_HIGHEST_FIRST","destination_id":"549499","adults_number":"1","locale":"en_US","currency":"USD","children_ages":"4,0,15","price_min":"10","star_rating_ids":"3,4,5","price_max":"500",}
-
-# headers = {
-# 	"X-RapidAPI-Key": "b2c27ee596mshd8717ee1c0eba27p1c6ff4jsn1467f4601dfe",
-# 	"X-RapidAPI-Host": "hotels-com-provider.p.rapidapi.com"
-# }
-
-# response = requests.request("GET", url, headers=headers, params=querystring)
-
-# print(response.text)
-
 initialize_local_environment()
 
 setup_hovor_rasa("pizza", train=False)

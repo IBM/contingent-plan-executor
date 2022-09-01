f23beace4e50f2206adda01f1ba86413255b7a07
beckydvn <18rldv@queensu.ca>
2022-09-01 16:20:17 -0400

    use value/known assignments for contextoutdet

--- hovor/outcome_determiners/context_dependent_outcome_determiner.py
+++ hovor/outcome_determiners/context_dependent_outcome_determiner.py
@@ -4,15 +4,18 @@ from hovor.outcome_determiners.outcome_determiner_base import OutcomeDeterminerB
 class ContextDependentOutcomeDeterminer(OutcomeDeterminerBase):
     def rank_groups(self, outcome_groups, progress):
         ranked_groups = []
-
         for group in outcome_groups:
             outcome_description = progress.get_description(group.name)
+            entity_requirements = group.entity_requirements
             conditions = outcome_description["context"]
             evaluated_condition = True
-            for ctx_var, val in conditions.items():
-                if progress.actual_context._fields[ctx_var] != val:
-                    evaluated_condition = False
-                    break
+            for ctx_var, ctx_var_cfg in conditions.items():
+                if "value" in ctx_var_cfg:
+                    if progress.actual_context._fields[ctx_var] != ctx_var_cfg["value"]:
+                        evaluated_condition = False
+                        break
+                elif "known" in ctx_var_cfg:
+                    pass
             confidence = 1.0 if evaluated_condition else 0.0
             ranked_groups.append((group, confidence))
         return ranked_groups, progress

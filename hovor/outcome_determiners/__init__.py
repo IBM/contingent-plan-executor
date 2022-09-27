bf73e5069e637b1f60a7561a561fe87716ac4273
beckydvn <18rldv@queensu.ca>
2022-09-26 21:49:18 -0400

    delete python call

--- hovor/outcome_determiners/__init__.py
+++ hovor/outcome_determiners/__init__.py
@@ -8,9 +8,4 @@ all_entities = {}
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
-try:
-    SPACY_LABELS = spacy.load("en_core_web_md").get_pipe("ner").labels
-except OSError:
-    # download and wait for completion
-    subprocess.run(["python -m spacy download en_core_web_md"])
-    SPACY_LABELS = spacy.load("en_core_web_md").get_pipe("ner").labels
+SPACY_LABELS = spacy.load("en_core_web_md").get_pipe("ner").labels

c1aa69975bba42f3c64b07121f3022ea06b57ead
jlaframboise <jlaframboise02@gmail.com>
2022-12-21 17:14:05 -0500

    Revert local_main.py to its original state before the simulator to avoid changing others' workflows. Add a simulator specific script.

--- local_main.py
+++ local_main.py
@@ -1,11 +1,8 @@
-from hovor.core import run_interaction, simulate_interaction
+from hovor.core import run_interaction
 from local_run_utils import *
 
 def run_local_conversation(output_files_path):
-    simulate_interaction(initialize_local_run(output_files_path))
-
-def simulate_local_conversation(output_files_path):
-    simulate_interaction(initialize_local_run_simulated(output_files_path))
+    run_interaction(initialize_local_run(output_files_path))
 
 if __name__ == "__main__":
-    simulate_local_conversation("/home/jacob/Dev/plan4dial/plan4dial/local_data/gold_standard_bot/output_files")
+    run_local_conversation("C:\\Users\\Rebecca\\Desktop\\plan4dial\\plan4dial\\local_data\\rollout_no_system_icaps_bot_mini\\output_files")

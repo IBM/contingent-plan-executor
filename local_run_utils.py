bd96fa2a64df6ea854126f1d787f4a21ef0a68e1
beckydvn <18rldv@queensu.ca>
2023-01-25 10:54:46 -0500

    update run utils to fix rasa run

--- local_run_utils.py
+++ local_run_utils.py
@@ -1,13 +1,12 @@
 from hovor.configuration.json_configuration_provider import JsonConfigurationProvider
 from environment import initialize_local_environment, initialize_local_environment_simulated
 from time import sleep
-import os
 import requests
 import subprocess
 import jsonpickle
 
 def create_validate_json_config_prov(output_files_path):
-    configuration_provider = JsonConfigurationProvider(os.path.join(output_files_path, "data"))
+    configuration_provider = JsonConfigurationProvider(f"{output_files_path}/data")
     # test on recoded provider
     json = jsonpickle.encode(configuration_provider)
     configuration_provider = jsonpickle.decode(json)
@@ -16,11 +15,12 @@ def create_validate_json_config_prov(output_files_path):
 
 def run_rasa_model_server(output_files_path):
     subprocess.Popen(f"rasa run --enable-api -m {output_files_path}/nlu_model.tar.gz", shell=True)
+    sleep(3.0)
     while True:
         try:
             requests.post("http://localhost:5005/model/parse", json={"text": ""})
-        except ConnectionError:
-            sleep(0.1)
+        except requests.exceptions.ConnectionError:
+            sleep(1.0)
         else:
             break
 
